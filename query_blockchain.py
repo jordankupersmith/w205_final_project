eb429254bcd57f3120d7262a4f3193f9b57cdc41
jordankupersmith <jmkupe@berkeley.edu>
2017-03-23 15:10:14 -0300

    added the timestamp from blockchain variable

--- query_blockchain.py
+++ query_blockchain.py
@@ -1,5 +1,8 @@
+###Change the script so it is only querying blockchain.info one time for each variable instead of separately.
+
 from datetime import timedelta, date, datetime
 import urllib, json
+import time
 
 epoch = datetime.utcfromtimestamp(0)
 
@@ -10,6 +13,8 @@ def daterange(start_date, end_date):
 def unix_time_millis(d):
     dt = datetime(d.year, d.month, d.day)
     return (dt - epoch).total_seconds() * 1000.0
+    
+
 
 def get_day_blocks(day_ms):
     url = "https://blockchain.info/blocks/{:.0f}?format=json"
@@ -32,6 +37,18 @@ def get_block_tx_count(block_hash):
 
     data = json.loads(response.read())
     return data['n_tx']
+    
+def get_timestamp(block_hash):
+    ###This returns the timestamp of the block standardized to UTC no matter what timezone the query is run in
+    url = "https://blockchain.info/rawblock/{0}"
+    response = urllib.urlopen(url.format(block_hash))
+    data = json.loads(response.read())
+    time_data=data['time']
+    is_dst = time.daylight and time.localtime().tm_isdst > 0
+    utc_offset = - (time.altzone if is_dst else time.timezone)
+    time_data_utc=time_data -utc_offset
+    return datetime.fromtimestamp(time_data_utc).strftime('%Y-%m-%d %H:%M:%S') 
+       
 
 
 if __name__ == "__main__": 
@@ -46,8 +63,9 @@ if __name__ == "__main__":
         day_tx_count = 0
         for i, block_hash in enumerate(block_hashes):
             block_tx_count = get_block_tx_count(block_hash)
-            print "block {} of {} has {} transactions".format(i, len(block_hashes), block_tx_count)
-            
+            timestamp = get_timestamp(block_hash)
+            #print "block {} of {} has {} transactions".format(i, len(block_hashes), block_tx_count)
+            print "block {} of {} has {} transactions and was processed at {}".format(i, len(block_hashes), block_tx_count, timestamp)
             day_tx_count += block_tx_count
         print single_date, day_tx_count
         date_count_list.append((single_date, day_tx_count))
