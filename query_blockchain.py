734916eb2150928b49bc24fcac6e58bf785f987c
jordankupersmith <jmkupe@berkeley.edu>
2017-03-24 12:35:07 -0300

    reduced the number of calls to blockchain.info and added the hash value so it can be a primary key

--- query_blockchain.py
+++ query_blockchain.py
@@ -21,6 +21,7 @@ def get_day_blocks(day_ms):
     response = urllib.urlopen(url.format(day_ms))
     data = json.loads(response.read())
     return data['blocks']
+    
 
 
 def get_block_hashes(day_ms):
@@ -31,41 +32,44 @@ def get_block_hashes(day_ms):
         block_hash_list.append(block['hash'])
     return block_hash_list
 
-def get_block_tx_count(block_hash):
+def get_block_info(block_hash):
     url = "https://blockchain.info/rawblock/{0}"
     response = urllib.urlopen(url.format(block_hash))
 
-    data = json.loads(response.read())
-    return data['n_tx']
+    block_variables = json.loads(response.read())
+    return block_variables
+  
+
     
 def get_timestamp(block_hash):
-    ###This returns the timestamp of the block standardized to UTC no matter what timezone the query is run in
-    url = "https://blockchain.info/rawblock/{0}"
-    response = urllib.urlopen(url.format(block_hash))
-    data = json.loads(response.read())
-    time_data=data['time']
+    time_data=block_variables['time']
     is_dst = time.daylight and time.localtime().tm_isdst > 0
     utc_offset = - (time.altzone if is_dst else time.timezone)
     time_data_utc=time_data -utc_offset
     return datetime.fromtimestamp(time_data_utc).strftime('%Y-%m-%d %H:%M:%S') 
        
-
-
+#####A blocks block hash always identifies a single block uniquely. A block also always has a specific block height. However, it is not always #the case that a specific block height can identify a single block. Rather, two or more blocks might compete for a single position in the #blockchain.
 if __name__ == "__main__": 
 
-    start_date = date(2009, 1, 27)
-    end_date = date(2009, 1, 28)
+    start_date = date(2013, 1, 27)
+    end_date = date(2013, 1, 28)
     
     date_count_list = []
     for single_date in daterange(start_date, end_date):
+        print single_date, "single_date"
         day_ms = unix_time_millis(single_date)
+        print day_ms, "day_ms"
         block_hashes = get_block_hashes(day_ms)
         day_tx_count = 0
         for i, block_hash in enumerate(block_hashes):
-            block_tx_count = get_block_tx_count(block_hash)
+            block_variables=get_block_info(block_hash)
+            print block_variables['height']
+            hash_value=block_variables['hash']
+            #block_tx_count = get_block_tx_count(block_hash)
+            block_tx_count = block_variables['n_tx']
             timestamp = get_timestamp(block_hash)
             #print "block {} of {} has {} transactions".format(i, len(block_hashes), block_tx_count)
-            print "block {} of {} has {} transactions and was processed at {}".format(i, len(block_hashes), block_tx_count, timestamp)
+            print "block {} of {} has a hash of {} and has {} transactions and was processed at {}".format(i, len(block_hashes), hash_value, block_tx_count, timestamp)
             day_tx_count += block_tx_count
         print single_date, day_tx_count
         date_count_list.append((single_date, day_tx_count))
